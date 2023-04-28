525a607794ff93f95162ccd1272b7ad763fded89
Baltazar Biverstal <baltazar.biverstal@elev.ga.ntig.se>
2023-04-28 21:11:24 +0200

    klar

--- Pokemon_spel/Program.cs
+++ Pokemon_spel/Program.cs
@@ -2,11 +2,24 @@
 Enemy enemy = new();
 Player player = new();
 
+//använder array för att det är snabbare och att jag inte kommer lägga till mer i den efter skapelse
 string[] choices = { "start(1)", "quit(2)" };
 string[] choicesturn = { "attack(1)", "heal(2)", "buff(3)" };
 
 bool active = true;
 
+Console.WriteLine("Hej vill du ha en tutorial? y/n");
+if (answer() == "Y" || answer() == "YES")
+{
+    Console.WriteLine("okej så spelet kommer gå till att du får välja mellan 3 olicka moves attack som gör skada, heal som ger tillbaka hälsa och buff som kommer göra din nästkommande attack starkare");
+    Console.WriteLine("sen kommer motståndaren att slå dig och du kör tills någon har fått slut på hp");
+    Console.ReadLine();
+    Console.Clear();
+}
+if (answer() == "N" || answer() == "NO")
+{
+    Console.Clear();
+}
 while (true)
 {
     active = true;
@@ -124,7 +137,8 @@ string answer()
     if (active)
     {
         string answer = Console.ReadLine();
-        return answer;
+        string result = answer.ToUpper();
+        return result;
     }
     else
     {
